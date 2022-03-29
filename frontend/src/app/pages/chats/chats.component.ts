63a05e7a7c0faa86d39323f294dea6698b252928
Tugolbai Maksatbek uulu <mkstbkv751@gmail.com>
2022-03-29 23:37:24 +0600

    переподключение

--- frontend/src/app/pages/chats/chats.component.ts
+++ frontend/src/app/pages/chats/chats.component.ts
@@ -49,36 +49,37 @@ export class ChatsComponent implements OnInit, OnDestroy {
       }
     });
 
-    this.ws = new WebSocket('ws://localhost:8000/chat');
-    this.ws.onclose = () => {
-      console.log("ws closed");
-    };
-
-    this.ws.onmessage = event => {
-      const decodedMessage: ServerMessage = JSON.parse(event.data);
-      this.users.push(decodedMessage.messages.user)
-
-      if (decodedMessage.type === 'NEW_MESSAGE') {
-        this.messages = decodedMessage.messages.messages;
+    const startWebsocket = () => {
+      this.ws = new WebSocket('ws://localhost:8000/chat');
+      this.ws.onopen = () => {
+        this.ws.send(JSON.stringify({type: 'LOGIN', token: this.token}))
       }
 
-      if (decodedMessage.type === 'PREV_MESSAGES') {
-        this.messages = decodedMessage.messages.messages;
-        this.users = decodedMessage.messages.users;
-      }
+      this.ws.onmessage = event => {
+        const decodedMessage: ServerMessage = JSON.parse(event.data);
+        this.users.push(decodedMessage.messages.user)
 
-      if (decodedMessage.type === 'LOGOUT') {
-        this.users = decodedMessage.messages.users;
-      }
-    };
+        if (decodedMessage.type === 'NEW_MESSAGE') {
+          this.messages = decodedMessage.messages.messages;
+        }
 
-    this.ws.onopen = () => {
-      this.ws.send(JSON.stringify({type: 'LOGIN', token: this.token}))
+        if (decodedMessage.type === 'PREV_MESSAGES') {
+          this.messages = decodedMessage.messages.messages;
+          this.users = decodedMessage.messages.users;
+        }
+
+        if (decodedMessage.type === 'LOGOUT') {
+          this.users = decodedMessage.messages.users;
+        }
+      };
+
+      this.ws.onclose = () => {
+        console.log("ws closed");
+        setTimeout(startWebsocket, 5000)
+      };
     }
 
-    this.ws.close = () => {
-      this.ws.send(JSON.stringify({type: 'USER_LOGOUT', token: this.token}))
-    }
+    startWebsocket();
   }
 
   sendMessage() {
@@ -90,6 +91,7 @@ export class ChatsComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy() {
+    this.ws.send(JSON.stringify({type: 'USER_LOGOUT', token: this.token}));
     this.ws.close();
     this.userSub.unsubscribe();
   }
